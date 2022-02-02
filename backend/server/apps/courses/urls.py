e65cfc985455d2b9d09c31321fdf38391a452192
Luis Mendoza <lmendozadev@gmail.com>
2022-02-02 15:04:14 -0500

    [ADD] routers to serializater

---
+++ backend/server/apps/courses/urls.py
@@ -0,0 +1,9 @@
+from rest_framework.routers import SimpleRouter
+from courses.views import CourseByCategory, SingleCourseViewSet
+
+app_name = 'courses'
+router = SimpleRouter()
+router.register(r"category/(?P<slug>[^/.]+)", CourseByCategory)
+router.register(r"course/(?P<id>[^/.]+)", SingleCourseViewSet)
+
+urlpatterns = router.urls
