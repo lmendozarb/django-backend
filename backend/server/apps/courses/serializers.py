9a26653c5c9f8914d223c7b801435428227e25c5
Luis Mendoza <lmendozadev@gmail.com>
2022-02-02 15:03:58 -0500

    [ADD] serializater to courses

---
+++ backend/server/apps/courses/serializers.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+from courses.models import Category, Course, Classroom
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ["name"]
+
+
+class CourseSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Course
+        fields = ["name"]
+        read_only = True
+        editable = False
+
+class ClassroomSerializer(serializers.ModelSerializer):
+
+    course= CourseSerializer(many=False, read_only=True)
+
+    class Meta:
+        model = Classroom
+        fields = [
+            "internal_reference",
+            "available_vacancie",
+            "start_hour",
+            "end_hour",
+            "course",
+            "qty_session",
+        ]
+        read_only = True
+
+
+class SingleCourseSerializer(serializers.HyperlinkedModelSerializer):
+    course_internal_reference = CourseSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Classroom
+        fields = ["internal_reference", "max_vacancies", "start_hour", "end_hour", "qty_session", "is_active", "course_internal_reference"]
